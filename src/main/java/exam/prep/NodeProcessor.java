b47540a5e5946a2a1bb5bfb226446920341bc2d2
adrian.tomalla <adtoit00@hs-esslingen.de>
2018-06-30 23:24:23 +0200

    Added addNewNodeInOrder Functionality

--- src/main/java/exam/prep/NodeProcessor.java
+++ src/main/java/exam/prep/NodeProcessor.java
@@ -12,4 +12,29 @@ public class NodeProcessor {
         p.next = newNode;
     }
 
+    //Füge einen neuen Node in der richtigen Reihenfolge hinzu
+    // Reihenfolge aufsteigend
+    public void addNodeInOrder(Node head, Node newNode){
+        Node p = head;
+        Node prev = null;
+        while(p.next != null ){
+                if(p.data > newNode.data){     // jetziges element ist größer als das neue
+                    if(p == head){      // element am anfang hinzufügen
+                        newNode.next = head;
+                        head = newNode;
+                    }else{      // element in der mitte oder am ende
+                        prev.next = newNode;
+                        newNode.next = p;
+                    }
+                    prev = p;
+                    p = p.next;
+
+                }
+
+        }
+
+    }
+
+
+
 }
--- src/test/java/exam/prep/AppTest.java
+++ src/test/java/exam/prep/AppTest.java
@@ -35,4 +35,24 @@ public class AppTest
 
     }
 
+    @Test
+    public void addNodeInOrderTest(){
+        NodeProcessor processor = new NodeProcessor();
+        Node head = new Node(3);
+        Node secondNode = new Node(5);
+        Node thirdNode = new Node(4);
+        Node forthNode = new Node(2);
+
+        processor.addNodeInOrder(head,secondNode);
+        processor.addNodeInOrder(head,thirdNode);
+        processor.addNodeInOrder(head,forthNode);
+
+        assertTrue(head.data == 2);
+        assertTrue((head.next.data == 3));
+        assertTrue(head.next.next.data ==4);
+        assertTrue(head.next.next.next.data == 5);
+
+    }
+
+
 }
